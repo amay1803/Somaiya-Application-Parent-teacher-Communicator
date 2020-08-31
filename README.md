6ef8e590997521141cd556c9bfcd2dd13e25abec
Amay Naik <amaynaik18@gmail.com>
2020-08-31 18:40:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Somaiya Parents Teacher Communicator
+
+<img src="images/LoginActivity.jpg" width="150"> <img src="images/Dashboard.jpg" width="150"> <img src="images/Events.jpg" width="150">
---
+++ app/src/main/java/com/example/myapplication/URL_Class.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+public class URL_Class {
+
+    public final static String url="https://somaiyaptm.000webhostapp.com/";
+
+    public static String getUrl() {
+        return url;
+    }
+}
